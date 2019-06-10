d5ea2af91e734fa9e6cf0816d85ac0571162a58d
m2lovric <m2lovric@gmail.com>
2019-06-10 22:29:13 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                <li>Main</li>
+                <li><button>New element</button></li>      
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
